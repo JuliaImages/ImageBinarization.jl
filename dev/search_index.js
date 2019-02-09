af0abd0a3e7f4ef472524caa9d6ab65aea925276
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-09 10:33:19 +0000

    build based on 401adb0

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "ImageBinarization.jl Documentation",
+    "title": "ImageBinarization.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#ImageBinarization.jl-Documentation-1",
+    "page": "ImageBinarization.jl Documentation",
+    "title": "ImageBinarization.jl Documentation",
+    "category": "section",
+    "text": "binarize(::Otsu, ::AbstractArray{T,2}) where T <: AbstractGray\nbinarize!(::Otsu, ::AbstractArray{T,2}) where T <: AbstractGray\nbinarize(::Polysegment, ::AbstractArray{T,2}) where T <: AbstractGray\nbinarize!(::Polysegment, ::AbstractArray{T,2}) where T <: AbstractGray"
+},
+
+]}
