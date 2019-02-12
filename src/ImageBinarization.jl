5acb0969de75a41dd261da64f6fc3d928018844d
Robert Woods <git@robertwoods.me>
2019-02-12 13:21:45 +1030

    Add Sauvola binarization algorithm

--- src/ImageBinarization.jl
+++ src/ImageBinarization.jl
@@ -6,20 +6,24 @@ using ColorVectorSpace
 using LinearAlgebra
 using HistogramThresholding
 using Polynomials
+using Statistics
 
 abstract type BinarizationAlgorithm end
 struct Otsu <: BinarizationAlgorithm end
 struct Polysegment <: BinarizationAlgorithm end
+struct Sauvola <: BinarizationAlgorithm end
 
-
+include("util.jl")
 include("otsu.jl")
 include("polysegment.jl")
+include("sauvola.jl")
 
 export
 	# main functions
     binarize,
 	binarize!,
 	Otsu,
-	Polysegment
+	Polysegment,
+	Sauvola
 
 end # module
