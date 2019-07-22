dd362231bb7879aaaf59edef77af0693859cc89c
Johnny Chen <johnnychen94@hotmail.com>
2019-07-23 01:03:47 +0800

    temporarily comment the test codes

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,20 @@ using ImageBinarization
 using Test, TestImages, ColorTypes, ColorVectorSpace, FixedPointNumbers, Statistics
 
 @testset "ImageBinarization.jl" begin
-    include("polysegment.jl")
-    include("adaptive_threshold.jl")
-    include("unimodal.jl")
-    include("minimum.jl")
-    include("intermodes.jl")
-    include("minimum_error.jl")
-    include("moments.jl")
-    include("yen.jl")
-    include("balanced.jl")
-    include("otsu.jl")
-    include("entropy.jl")
-    include("util.jl")
-    include("sauvola.jl")
-    include("niblack.jl")
+    # include("util.jl")
+    #
+    # include("adaptive_threshold.jl")
+    # include("balanced.jl")
+    # include("entropy.jl")
+    # include("intermodes.jl")
+    # include("minimum.jl")
+    # include("minimum_error.jl")
+    # include("moments.jl")
+    # include("niblack.jl")
+    # include("otsu.jl")
+    # include("polysegment.jl")
+    # include("sauvola.jl")
+    # include("unimodal.jl")
+    # include("yen.jl")
+
 end
