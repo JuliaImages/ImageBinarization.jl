fcaacc4330b078421ef623048c2ba3343b7599e0
Johnny Chen <johnnychen94@hotmail.com>
2019-07-26 02:01:53 +0800

    refactor MinimumError using functor API
    This is a PR-commit for PR
    https://github.com/zygmuntszpak/ImageBinarization.jl/pull/29 that
    refactors the whole codebase to use the new functor API, which is
    discussed in more details in
    https://github.com/zygmuntszpak/ImageBinarization.jl/issues/26 and
    explained in my [GSoC 2019
    blog](https://nextjournal.com/johnnychen94/the-principles-of-imagesjl-part-i)
    
    Changes:
    
    * support `Color3` input
    * move implementations into functor
    * enhance the test codes with wider test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,13 @@ include("testutils.jl")
 
 @testset "ImageBinarization.jl" begin
     include("util.jl")
-  
+
     include("adaptive_threshold.jl")
     # include("balanced.jl")
     # include("entropy.jl")
     # include("intermodes.jl")
     # include("minimum.jl")
-    # include("minimum_error.jl")
+    include("minimum_error.jl")
     # include("moments.jl")
     # include("niblack.jl")
     # include("otsu.jl")
