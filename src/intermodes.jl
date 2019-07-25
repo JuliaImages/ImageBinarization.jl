67d3cfb5d1c870b22eead8cb112ac3d95f9de8b9
Johnny Chen <johnnychen94@hotmail.com>
2019-07-26 01:29:14 +0800

    refactor Intermodes using the functor API
    This is a PR-commit for PR
    https://github.com/zygmuntszpak/ImageBinarization.jl/pull/29 that
    refactors the whole codebase to use the new functor API, which
    is discussed in more details in
    https://github.com/zygmuntszpak/ImageBinarization.jl/issues/26 and
    explained in my [GSoC 2019
    blog](https://nextjournal.com/johnnychen94/the-principles-of-imagesjl-part-i)
    
    Changes:
    
    * support `Color3` input
    * move implementations into functor
    * enhance the test codes with wider test coverage

--- src/intermodes.jl
+++ src/intermodes.jl
@@ -1,10 +1,10 @@
-struct Intermodes <: AbstractImageBinarizationAlgorithm end
-
-
 """
-```
-binarize(Intermodes(), img)
-```
+    Intermodes <: AbstractImageBinarizationAlgorithm
+    Intermodes()
+
+    binarize([T,] img, f::AdaptiveThreshold; [window_size])
+    binarize!([out,] img, f::AdaptiveThreshold; [window_size])
+
 
 Under the assumption that the image histogram is bimodal the image histogram is
 smoothed using a length-3 mean filter until two modes remain. The binarization
@@ -12,15 +12,16 @@ threshold is then set to the average value of the two modes.
 
 # Output
 
-Returns the binarized image as an `Array{Gray{Bool},2}`.
+Return the binarized image as an `Array{Gray{T}}` of size `size(img)`. If
+`T` is not specified, it is inferred from `out` and `img`.
 
 # Arguments
 
 The function argument is described in more detail below.
 
-##  `img`
+##  `img::AbstractArray`
 
-An `AbstractArray` representing an image. The image is automatically converted
+The image that needs to be binarized. The image is automatically converted
 to `Gray` in order to construct the requisite graylevel histogram.
 
 # Example
@@ -31,19 +32,23 @@ Binarize the "cameraman" image in the `TestImages` package.
 using TestImages, ImageBinarization
 
 img = testimage("cameraman")
-img_binary = binarize(Intermodes(), img)
+img_binary = binarize(img, Intermodes())
 ```
 
 ## Reference
 
 1. C. A. Glasbey, “An Analysis of Histogram-Based Thresholding Algorithms,” CVGIP: Graphical Models and Image Processing, vol. 55, no. 6, pp. 532–537, Nov. 1993. [doi:10.1006/cgip.1993.1040](https://doi.org/10.1006/cgip.1993.1040)
 """
-function binarize(algorithm::Intermodes,  img::AbstractArray{T,2}) where T <: Colorant
-    img₀₁ = zeros(Gray{Bool}, axes(img))
+struct Intermodes <: AbstractImageBinarizationAlgorithm end
+
+function (f::Intermodes)(out::GenericGrayImage, img::GenericGrayImage)
     edges, counts = build_histogram(img,  256)
     t = find_threshold(HistogramThresholding.Intermodes(), counts[1:end], edges)
-    for i in CartesianIndices(img)
-      img₀₁[i] = img[i] < t ? 0 : 1
+    @simd for i in CartesianIndices(img)
+      out[i] = img[i] < t ? 0 : 1
     end
-    img₀₁
+    out
 end
+
+(f::Intermodes)(out::GenericGrayImage, img::AbstractArray{<:Color3}) =
+    f(out, of_eltype(Gray, img))
